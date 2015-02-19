47ab371297268f5f7f9f4e16e5c523c189acce07
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-19 13:17:54 +0000

    Added Video_Recording table.

---
+++ ForestData/Video_Recording.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Video_Recording] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Artist]       NVARCHAR (50) NULL,
+    [Title]        NVARCHAR (50) NULL,
+    [Genre]        NCHAR (10)    NULL,
+    [Video_Length] INT           NULL,
+    [Quality_Type] NCHAR (10)    NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
