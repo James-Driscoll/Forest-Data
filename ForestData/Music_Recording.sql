6034d6d52af2ab519d7584b8d4407d290268db6d
james-driscoll <james.e.m.driscoll@gmail.com>
2015-02-24 17:54:54 +0000

    Simplified Music_Recording data types to just strings, ints and dates.

--- ForestData/Music_Recording.sql
+++ ForestData/Music_Recording.sql
@@ -1,12 +1,12 @@
 ﻿CREATE TABLE [dbo].[Music_Recording] (
     [Id]          INT           IDENTITY (1, 1) NOT NULL,
-    [Artist]      NVARCHAR (50) NULL,
-    [Title]       NVARCHAR (50) NULL,
+    [Artist]      NVARCHAR (50) NOT NULL,
+    [Title]       NVARCHAR (50) NOT NULL,
     [Genre]       INT           NOT NULL,
-    [Image_Name]  NVARCHAR (50) NULL,
-    [Num_Tracks]  INT           NULL,
-    [Price]       FLOAT (53)    NULL,
-    [Stock_Count] INT           NULL,
-    [Released]    DATE          NULL,
+    [Image_Name]  NVARCHAR (50) NOT NULL,
+    [Num_Tracks]  INT           NOT NULL,
+    [Price]       INT    NOT NULL,
+    [Stock_Count] INT           NOT NULL,
+    [Released]    DATE          NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
